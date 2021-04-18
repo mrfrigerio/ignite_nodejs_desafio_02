0d986d2aeaf61e5a0acedb1a35fa6fadb64cd2d0
Marcelo Ragnelli Frigerio <mrfrigerio@yahoo.com.br>
2021-04-18 15:12:01 -0300

    Implementadas todas as features do desafio

--- src/index.js
+++ src/index.js
@@ -10,19 +10,68 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers
+  const userExists = users.find((user) => user.username === username)
+
+  if(!userExists){
+    return response.status(404).json({ error: "User not found!" })
+  }
+
+  if(!validate(userExists.id)) {
+    return response.status(403).json({error: "invalid user uuid!"})
+  }
+
+  request.user = userExists
+  return next()
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request
+  if(!user.pro && user.todos.length >= 10) {
+    return response.status(403).json({error: "Todo limit exceeded. Please purchase a pro plan!"})
+  }
+  return next()
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers
+  const { id } = request.params
+
+  const userExists = users.find((user) => user.username === username)
+
+  if(!userExists){
+    return response.status(404).json({ error: "User not found!" })
+  }
+
+  if(!validate(id)) {
+    return response.status(400).json({error: "invalid todo uuid!"})
+  }
+
+  if(!validate(userExists.id)) {
+    return response.status(400).json({error: "invalid user uuid!"})
+  }
+
+  const todo = userExists.todos.find(t => t.id === id)
+
+  if(!todo) {
+    return response.status(404).json({error: "Todo not found!"})
+  }
+
+  request.user = userExists
+  request.todo = todo
+  return next()
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params
+  const userExists = users.find((user) => user.id === id)
+
+  if(!userExists){
+    return response.status(404).json({ error: "User not found!" })
+  }
+
+  request.user = userExists
+  return next()
 }
 
 app.post('/users', (request, response) => {
